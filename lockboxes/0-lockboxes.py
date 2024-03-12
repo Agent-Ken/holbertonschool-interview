627256e84e9558b4e047a7012d00bc3816ec710d
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-13 03:36:09 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 
-''' Documentation part:
-The main function determines if all the boxes can be opened.
-`boxes (List[List[int]]):` - list of lists where each inner list represents a box and contains keys to other boxes.
-Returns either true or false depending on whether all the boxes can be opened or not
-The first box is unlocked by default.
-
-"Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures". It is better than "breadth-first search".
-We use it to explore reachable boxes. In it the `box_index (int)` is the index of the current box. At all we check if all boxes were visited or not.
-'''
+"""
+The approach here is based on tracking reachable boxes using a set and verifying that all boxes are accessible from the initial box.
+If any box cannot be unlocked, the function returns `False`, otherwise it returns `True`.
+"""
 
 def canUnlockAll(boxes):
-    box_amount = len(boxes)
-    visited = [False] * box_amount
-    visited[0] = True  
+    openBoxes = set()
+    for i, key in enumerate(boxes):
+        for box in key:
+            if box < len(boxes) and box != i:
+                openBoxes.add(box)
+    openBoxes.add(0)
 
-    def depth_first_search(box_index):
-        for key in boxes[box_index]:
-            if not visited[key]:
-                visited[key] = True
-                depth_first_search(key)
-
-    depth_first_search(0)
-    return all(visited)
+    for index, openBox in enumerate(openBoxes):
+        if index != openBox:
+            return False
+    return True
