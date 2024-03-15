038a7142cdb9684a3cb468c21e043f22b04c98a4
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-15 12:39:28 +0400

    Add files via upload

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -11,22 +11,22 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode;
+binary_tree_t *newNode;
 
-    newNode = malloc(sizeof(binary_tree_t));
+newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
+if (newNode == NULL)
+{
+return (NULL);
+}
 
-    newNode->n = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->parent = NULL;
+newNode->n = value;
+newNode->left = NULL;
+newNode->right = NULL;
+newNode->parent = NULL;
 
-    if (parent != NULL)
-        newNode->parent = parent;
+if (parent != NULL)
+newNode->parent = parent;
 
-    return (newNode);
+return (newNode);
 }
