fcaaefa74af21e982ef3df3d8cdf6634c5066b55
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-22 13:27:55 +0400

    Add files via upload

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,28 +6,27 @@ Main file for testing
 
 
 def minOperations(n):
+    """
+    Calculates the minimum number of operations to reach 'n' from 0.
+    :param n: Target number
+    :return: Minimum number of operations
+    """
     if n == 1 or n <= 0:
         return 0
-    lngth = 0
+
     min_op = 0
-    for i in range(2, n//2+2):
+    for i in range(2, n // 2 + 1):
         if n % i == 0:
             min_op = i
             break
 
     count = min_op
-    lngth = min_op
 
-    while n != lngth:
-        if n % lngth == 0:
-            min_op = lngth
-            lngth += min_op
-
+    while n != count:
+        if n % count == 0:
+            min_op = count
             count += 2
-
         else:
             count += 1
-            lngth += min_op
 
     return count
-    
