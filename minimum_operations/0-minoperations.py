8f5703f71d50ef8b3278c7ea32832f485d0f3d0a
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-22 13:20:11 +0400

    Add files via upload

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Main file for testing
+"""
+
+
+def minOperations(n):
+    
+    if n == 1 or n <= 0:
+        return 0
+    
+    lngth, min_op = 0
+    
+    for i in range(2, n//2+2):
+        if n % i == 0:
+            min_op = i
+            break
+
+    count, lngth = min_op
+
+    while n != lngth:
+        
+        if n % lngth == 0:
+            min_op = lngth
+            lngth += min_op
+
+            count += 2
+
+        else:
+            count += 1
+            lngth += min_op
+
+    return count
