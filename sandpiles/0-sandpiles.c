c785da18619738bef13760964090979a4d5fbadb
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-29 20:23:26 +0400

    Add files via upload

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,83 +1,134 @@
+#include "sandpiles.h"
 #include <stdio.h>
+
 /**
- * stable - Checks if sandpile is stable
+ * print_grid - Print a 3x3 grid
  * @grid: 3x3 grid to be printed
- * Return: Is sandpile stable or not(1,0)
- */
-int stable(int grid[3][3])
-{
-int i, j;
-for (i = 0; i < 3; i++)
-{
-for (j = 0; j < 3; j++)
-{
-if (grid[i][j] > 3)
-return (0);
-}
-}
-return (1);
-}
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
  */
 static void print_grid(int grid[3][3])
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i < 3; i++)
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * topple - Topple a cell in the sandpile
+ * @grid: 3x3 grid
+ * @row: Row of the cell to be toppled
+ * @col: Column of the cell to be toppled
+ */
+static void topple(int grid[3][3], int row, int col)
 {
-for (j = 0; j < 3; j++)
+	grid[row][col] -= 4;
+	if (row - 1 >= 0)
+		grid[row - 1][col]++;
+	if (row + 1 < 3)
+		grid[row + 1][col]++;
+	if (col - 1 >= 0)
+		grid[row][col - 1]++;
+	if (col + 1 < 3)
+		grid[row][col + 1]++;
+}
+
+/**
+ * add_grids - Add two grids element-wise
+ * @grid1: 3x3 grid to store the sum
+ * @grid2: 3x3 grid to be added to grid1
+ */
+static void add_grids(int grid1[3][3], int grid2[3][3])
 {
-if (j)
-printf(" ");
-printf("%d", grid[i][j]);
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 }
-printf("\n");
+
+/**
+ * mark_unstable - Mark unstable cells in the grid
+ * @grid: 3x3 grid to be checked
+ * @unstable: 3x3 grid to store unstable cell markers
+ *
+ * Return: 1 if the grid is unstable, 0 otherwise
+ */
+static int mark_unstable(int grid[3][3], int unstable[3][3])
+{
+	int i, j, is_unstable = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				is_unstable = 1;
+				unstable[i][j] = 1;
+			}
+			else
+			{
+				unstable[i][j] = 0;
+			}
+		}
+	}
+
+	return (is_unstable);
 }
+
+/**
+ * topple_unstable - Topple all unstable cells in the grid
+ * @grid: 3x3 grid to be toppled
+ * @unstable: 3x3 grid with unstable cell markers
+ */
+static void topple_unstable(int grid[3][3], int unstable[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (unstable[i][j])
+			{
+				topple(grid, i, j);
+				unstable[i][j] = 0;
+			}
+		}
+	}
 }
+
 /**
  * sandpiles_sum - Computes the sum of two sandpiles
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- * Return: void
-*/
+ * @grid1: 3x3 grid representing the first sandpile
+ * @grid2: 3x3 grid representing the second sandpile
+ *
+ * Description: The function computes the sum of two sandpiles and stabilizes
+ * the resulting sandpile by performing toppling operations as needed.
+ * The final stable sandpile is stored in grid1.
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-int i, j;
-for (i = 0; i < 3; i++)
-for (j = 0; j < 3; j++)
-{
-grid1[i][j] += grid2[i][j];
-grid2[i][j] = 0;
-}
-if (stable(grid1))
-return;
-while (!stable(grid1))
-{
-printf("=\n");
-print_grid(grid1);
-for (i = 0; i < 3; i++)
-for (j = 0; j < 3; j++)
-if (grid1[i][j] > 3)
-{
-grid2[i][j] -= 4;
-if (i - 1 >= 0)
-grid2[i - 1][j] += 1;
-if (i + 1 < 3)
-grid2[i + 1][j] += 1;
-if (j - 1 >= 0)
-grid2[i][j - 1] += 1;
-if (j + 1 < 3)
-grid2[i][j + 1] += 1;
-}
-for (i = 0; i < 3; i++)
-{
-for (j = 0; j < 3; j++)
-{
-grid1[i][j] += grid2[i][j];
-grid2[i][j] = 0;
-}
-}
-};
+	int unstable[3][3];
+
+	add_grids(grid1, grid2);
+
+	while (mark_unstable(grid1, unstable))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		topple_unstable(grid1, unstable);
+	}
 }
