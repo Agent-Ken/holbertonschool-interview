d664babceb9a06b5ac6dafd8cba2583506e7aa6d
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-29 19:20:02 +0400

    Add files via upload

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -3,13 +3,15 @@
 
 static void print_grid(int grid[3][3])
 {
-    for (int i = 0; i < 3; i++)
+    int i, j;
+
+    for (i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
-            printf("%d", grid[i][j]);
-            if (j < 2)
+            if (j)
                 printf(" ");
+            printf("%d", grid[i][j]);
         }
         printf("\n");
     }
@@ -30,30 +32,48 @@ static void topple(int grid[3][3], int row, int col)
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    for (int i = 0; i < 3; i++)
+    int i, j;
+    int stable = 0;
+    int unstable[3][3] = {{0}};
+
+    for (i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
             grid1[i][j] += grid2[i][j];
         }
     }
 
-    int stable;
-    do
+    while (!stable)
     {
         stable = 1;
-        for (int i = 0; i < 3; i++)
+        for (i = 0; i < 3; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (j = 0; j < 3; j++)
             {
                 if (grid1[i][j] > 3)
                 {
                     stable = 0;
-                    printf("=\n");
-                    print_grid(grid1);
-                    topple(grid1, i, j);
+                    unstable[i][j] = 1;
                 }
             }
         }
-    } while (!stable);
+
+        if (!stable)
+        {
+            printf("=\n");
+            print_grid(grid1);
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (unstable[i][j])
+                    {
+                        topple(grid1, i, j);
+                        unstable[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
 }
