39e0525481bf80667e91be93c6a2995f38676ecc
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-03-29 19:50:21 +0400

    Fixing the stupid 40 lines Betty warning

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -41,6 +41,54 @@ static void topple(int grid[3][3], int row, int col)
 }
 
 /**
+ * add_grids - Add two grids element-wise
+ * @grid1: 3x3 grid to store the sum
+ * @grid2: 3x3 grid to be added to grid1
+ */
+static void add_grids(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+}
+
+/**
+ * mark_unstable - Mark unstable cells in the grid
+ * @grid: 3x3 grid to be checked
+ * @unstable: 3x3 grid to store unstable cell markers
+ *
+ * Return: 1 if the grid is unstable, 0 otherwise
+ */
+static int mark_unstable(int grid[3][3], int unstable[3][3])
+{
+	int i, j, unstable_found = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				unstable_found = 1;
+				unstable[i][j] = 1;
+			}
+			else
+			{
+				unstable[i][j] = 0;
+			}
+		}
+	}
+
+	return unstable_found;
+}
+
+/**
  * sandpiles_sum - Computes the sum of two sandpiles
  * @grid1: 3x3 grid representing the first sandpile
  * @grid2: 3x3 grid representing the second sandpile
@@ -51,47 +99,35 @@ static void topple(int grid[3][3], int row, int col)
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int unstable[3][3];
+
+	add_grids(grid1, grid2);
+
+	while (mark_unstable(grid1, unstable))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		topple_unstable(grid1, unstable);
+	}
+}
+
+/**
+ * topple_unstable - Topple all unstable cells in the grid
+ * @grid: 3x3 grid to be toppled
+ * @unstable: 3x3 grid with unstable cell markers
+ */
+static void topple_unstable(int grid[3][3], int unstable[3][3])
+{
 	int i, j;
-	int stable = 0;
-	int unstable[3][3] = {{0}};
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] += grid2[i][j];
-		}
-	}
-
-	while (!stable)
-	{
-		stable = 1;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
+			if (unstable[i][j])
 			{
-				if (grid1[i][j] > 3)
-				{
-					stable = 0;
-					unstable[i][j] = 1;
-				}
-			}
-		}
-
-		if (!stable)
-		{
-			printf("=\n");
-			print_grid(grid1);
-			for (i = 0; i < 3; i++)
-			{
-				for (j = 0; j < 3; j++)
-				{
-					if (unstable[i][j])
-					{
-						topple(grid1, i, j);
-						unstable[i][j] = 0;
-					}
-				}
+				topple(grid, i, j);
+				unstable[i][j] = 0;
 			}
 		}
 	}
