49ad4fbc3408d8a5e9caea3f15db7eff2cb0a9f5
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-04-12 13:16:14 +0400

    Update 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-"""
-log_pattern - Define the log pattern and compile it for better performance
-total_size & status_codes - Initialize variables to store the total size and status code counts
-print_stats() - Function to print the statistics
-signal.signal() - Set the signal handler for SIGINT (CTRL + C)
-And at the end - read from stdin line by line
-"""
+
+"""Everything is clear probably"""
+
 
 import sys
 import re
