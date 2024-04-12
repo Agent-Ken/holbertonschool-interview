210968328abbe1a9e573c2281a29f06aef9c23c1
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-04-12 16:58:48 +0400

    TEST

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line by line and computes metrics
-"""
+
+
+"""Input stats"""
+
+
 import sys
-
-code_status = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0}
-
-# Variable to store the total file size
-total_sizes = 0
-# Counter to keep track of the number of lines read
-count_line = 1
+import re
 
 
-def printStats():
-    """
-    Print the final statistics after all lines have been read
-    """
-    print('File size: {}'.format(total_sizes))
-    for code in sorted(code_status.keys()):
-        if code_status[code] != 0:
-            print('{}: {}'.format(code, code_status[code]))
+logs = 0
+total_size = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+
+def print_statistics(statuses, total):
+    print("File size: {}".format(total))
+    for key, value in sorted(statuses.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
 try:
-    # Read lines from stdin one by one
     for line in sys.stdin:
-        try:
-            line = line[:-1]
-            parts = line.split(' ')
-            total_sizes += int(parts[-1])
-            status_code = int(parts[-2])
-            if status_code in code_status:
-                code_status[status_code] += 1
-        except Exception:
-            # Skip the line if it is not in the expected format
-            pass
-
-        if count_line % 10 == 0:
-            printStats()
-        count_line += 1
-
-except KeyboardInterrupt:
-    printStats()
-    raise
-printStats()
+        new_line = line.rstrip().split(" ")
+        if len(new_line) == 9 or len(new_line) == 7:
+            try:
+                logs += 1
+                total_size += int(new_line[-1])
+                status_codes[new_line[-2]] += 1
+                if logs % 10 == 0 and logs != 0:
+                    print_statistics(status_codes, total_size)
+            except BaseException:
+                pass
+finally:
+    print_statistics(status_codes, total_size)
