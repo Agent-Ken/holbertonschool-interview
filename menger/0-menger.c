609a025281144017c6453562e4b2b0af00090fe7
agent-ken <gbp.kennenson@gmail.com>
2024-05-24 18:24:38 +0000

    betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -10,13 +10,13 @@
  */
 int is_block_empty(int row, int col)
 {
-    while (row > 0 || col > 0)
-    {
-        if (row % 3 == 1 && col % 3 == 1)
-            return (1);
-        row /= 3, col /= 3;
-    }
-    return (0);
+	while (row > 0 || col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return (1);
+		row /= 3, col /= 3;
+	}
+	return (0);
 }
 
 /**
@@ -25,18 +25,18 @@ int is_block_empty(int row, int col)
  */
 void menger(int level)
 {
-    int size = pow(3, level);
-    int row, col;
+	int size = pow(3, level);
+	int row, col;
 
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            if (is_block_empty(row, col))
-                printf(" ");
-            else
-                printf("#");
-        }
-        printf("\n");
-    }
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			if (is_block_empty(row, col))
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
 }
