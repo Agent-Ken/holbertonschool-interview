cc479e02c43d9d0f4762bd2385a74f355bb9a814
agent-ken <gbp.kennenson@gmail.com>
2024-08-02 18:52:15 +0000

    fix

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,106 +1,150 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <holberton.h>
 
 /**
- * _isdigit - checks if a string is a number
- * @str: string to check
- *
- * Return: 1 if str is a number, 0 otherwise
- */
-int _isdigit(char *str)
-{
-	int i = 0;
-
-	if (str[0] == '-')
-		i = 1;
-	for (; str[i]; i++)
-	{
-		if (!isdigit(str[i]))
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * _puts - prints a string
- * @str: string to print
- *
- * Return: void
- */
-void _puts(char *str)
-{
-	while (*str)
-	{
-		putchar(*str);
-		str++;
-	}
-	putchar('\n');
-}
-
-/**
- * main - multiplies two large numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: 0 on success, 98 on error
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int len1, len2, len, i, j, carry, n1, n2, *result;
-	char *num1, *num2;
+	char *result;
 
-	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	if (argc != 3)
 	{
 		_puts("Error");
 		exit(98);
 	}
 
-	num1 = argv[1];
-	num2 = argv[2];
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	len = len1 + len2;
-	result = calloc(len, sizeof(int));
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		_puts("Error");
+		exit(98);
+	}
 
+	result = multiply(argv[1], argv[2]);
 	if (result == NULL)
 	{
 		_puts("Error");
 		exit(98);
 	}
 
-	for (i = len1 - 1; i >= 0; i--)
+	if (result[0] == '0' && result[1] == '\0')
 	{
-		if (num1[i] == '-')
-			continue;
-		n1 = num1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			if (num2[j] == '-')
-				continue;
-			n2 = num2[j] - '0';
-			carry += result[i + j + 1] + (n1 * n2);
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry)
-			result[i + j + 1] += carry;
+		_putchar('0');
+		_putchar('\n');
 	}
-
-	i = 0;
-	while (i < len && result[i] == 0)
-		i++;
-	if (i == len)
-		putchar('0');
 	else
 	{
-		for (; i < len; i++)
-			putchar(result[i] + '0');
+		print_result(result);
 	}
-	putchar('\n');
 	free(result);
+
 	return (0);
+}
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @s: string to check
+ * Return: 1 if only digits, 0 otherwise
+ */
+int is_digit(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
+
+/**
+ * multiply - multiplies two numbers represented as strings
+ * @num1: first number
+ * @num2: second number
+ * Return: result of multiplication or NULL on failure
+ */
+char *multiply(char *num1, char *num2)
+{
+	int len1, len2, len, i, j, carry;
+	int *result;
+	char *str_result;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
+
+	result = calloc(len, sizeof(int));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+		}
+	}
+
+	for (i = len - 1; i > 0; i--)
+	{
+		carry = result[i] / 10;
+		result[i - 1] += carry;
+		result[i] %= 10;
+	}
+
+	str_result = malloc(len + 1);
+	if (str_result == NULL)
+	{
+		free(result);
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+		str_result[i] = result[i] + '0';
+	str_result[len] = '\0';
+
+	while (*str_result == '0' && *(str_result + 1) != '\0')
+		str_result++;
+
+	free(result);
+	return (str_result);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to measure
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * print_result - prints the result
+ * @result: string to print
+ */
+void print_result(char *result)
+{
+	while (*result)
+		_putchar(*result++);
+	_putchar('\n');
 }
--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -1,7 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _isdigit(char *str);
+int _putchar(char c);
 void _puts(char *str);
+char *multiply(char *num1, char *num2);
+int _strlen(char *s);
+void print_result(char *result);
+int is_digit(char *s);
 
 #endif
