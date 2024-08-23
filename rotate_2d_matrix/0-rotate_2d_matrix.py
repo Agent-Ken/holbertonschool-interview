58bfe76535668fd42c3cca09a3c12616c3713df6
agent-ken <gbp.kennenson@gmail.com>
2024-08-23 18:51:03 +0000

    pep8 validation

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,21 +3,22 @@
 Module to rotate a 2D matrix 90 degrees clockwise.
 """
 
+
 def rotate_2d_matrix(matrix):
-	"""
-	Rotates a 2D matrix 90 degrees clockwise.
+    """
+    Rotates a 2D matrix 90 degrees clockwise.
 
-	Args:
-		matrix (list of list of int): 2D matrix to be rotated.
+    Args:
+        matrix (list of list of int): 2D matrix to be rotated.
 
-	Returns:
-		None: The matrix is modified in place.
-	"""
-	n = len(matrix)
-	for i in range(n // 2):
-		for j in range(i, n - i - 1):
-			temp = matrix[i][j]
-			matrix[i][j] = matrix[n - j - 1][i]
-			matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
-			matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
-			matrix[j][n - i - 1] = temp
+    Returns:
+        None: The matrix is modified in place.
+    """
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
