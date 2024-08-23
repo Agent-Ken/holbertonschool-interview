a06e3112a9ac5476fd8ce00e2e01b454c1501cb8
agent-ken <gbp.kennenson@gmail.com>
2024-08-23 18:45:41 +0000

    update

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -9,29 +9,29 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-    if (head == NULL || head->next == NULL)
-        return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    tortoise = head->next;
-    hare = head->next->next;
+	tortoise = head->next;
+	hare = head->next->next;
 
-    while (hare && hare->next)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
-            while (tortoise != hare)
-            {
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-            return (tortoise);
-        }
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-    }
+	while (hare && hare->next)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
